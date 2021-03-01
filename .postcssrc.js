8397d88e0f6f08a097d3df4f43a5e1ce8aa71874
even <731788284@qq.com>
2021-03-01 15:59:54 +0800

    优化css配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,20 @@
 module.exports = {
   plugins: {
-    'autoprefixer': {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // autoprefixer 自动为css添加浏览器兼容前缀
+    // 属于postcss其中的一个工具
+    // vuecli 默认集成autoprefixer插件 无需再配置
+    // 'autoprefixer': {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
+
+    // 配置使用 postcss-pxtorem 插件
+    // 作用：把 px 转为 rem
     'postcss-pxtorem': {
-      rootValue: 37.5,
+      rootValue ({ file }) {
+        // vant样式 1rem = 37.5px 
+        // 其他样式 1rem = 75px
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
       propList: ['*']
     }
   }
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
     <h2>蛤蟆头条</h2>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
 
 <style lang="less">
+h2 {
+  padding: 75px;
+}
 </style>
