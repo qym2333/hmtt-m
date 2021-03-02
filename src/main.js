f3c9b722c4f5dca8ba7c3e8dac8861eb0ad74b7c
even <731788284@qq.com>
2021-03-02 11:47:27 +0800

    登录页结构和表单验证

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 全局样式
-import './styles/index.less'
 // import Vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 全局样式
+import './styles/index.less'
 // rem适配 动态设置html根节点大小
 import 'amfe-flexible'
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,13 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login')
+  }
+]
 
 const router = new VueRouter({
   routes
